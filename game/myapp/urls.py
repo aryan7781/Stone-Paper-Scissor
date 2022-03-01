06fd9478fd5a17dccc6538bf4f8cd68cae188115
Naman82 <b520031@iiit-bh.ac.in>
2022-03-01 20:46:39 +0530

    camera integrated

--- game/myapp/urls.py
+++ game/myapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns=[
     path('play',views.play,name="play"),
     path('profile',views.profile,name="profile"),
     path('single_player',views.singleplayer,name="single_player"),
-    path('multi_player',views.multiplayer,name="multi_player")
+    path('multi_player',views.multiplayer,name="multi_player"),
+    path('video_feed',views.video_feed,name='video_feed'),
 ]
--- game/myapp/views.py
+++ game/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.http import StreamingHttpResponse
+from myapp.camera import VideoCamera
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -18,3 +19,13 @@ def singleplayer(request):
 
 def multiplayer(request):
     return render(request, 'multi_player.html')
+
+def gen(camera):
+    while True:
+        frame=camera.get_frame()
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+def video_feed(request):
+    return StreamingHttpResponse(gen(VideoCamera()),content_type='multipart/x-mixed-replace; boundary=frame')
